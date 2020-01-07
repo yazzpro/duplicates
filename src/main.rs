c6988928f992ab86fa5bf2bcfd9810911227cf18
Jarosław Pawlak <jaroslaw.pawlak@gmail.com>
2020-01-07 06:30:59 +0100

    calculating hash and file size

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-extern crate crypto;
+use crypto::sha2::Sha512;
+use crypto::digest::Digest;
 
 use std::fs;
+use std::fs::File;
 use std::path::PathBuf;
-
+use std::io::{BufReader, Read};
 
 #[derive(Debug)]
 struct FileInfo {
     full_path: String,
-    size: usize,
+    size: u64,
     hash: String
 }
 
@@ -18,12 +20,30 @@ fn main() {
 
     let srcdir = PathBuf::from(&a1);
     let full_path = fs::canonicalize(&srcdir).expect("File could not be processed");
+    let file = File::open(&full_path).expect("can't upen file");
+    let file_length = file.metadata().unwrap().len();
+    let mut reader = BufReader::new(file);
 
+    let mut hasher = Sha512::new();
+    
+    let mut buffer = [0; 4096];
+
+    loop {
+        let count = reader.read(&mut buffer).expect("what kind of error can happen on reading buffer?");
+        if count == 0 {
+            break;
+        }
+        hasher.input(&buffer[0..count]);
+    }    
+    
+    let digest = hasher.result_str();
+
+    println!("digest is {:?}",&digest);
 
     let f = FileInfo {
         full_path : full_path.to_str().expect("Path could not be translated").to_string(),
-        size : 1,
-        hash : "hash".to_string()
+        size : file_length,
+        hash : digest
     };
     println!("{:?}", f);
 
