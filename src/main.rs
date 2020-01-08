00665dae27931ff8382c120d83dc7c9a20045026
Jarosław Pawlak <jaroslaw.pawlak@gmail.com>
2020-01-08 06:54:34 +0100

    processing directory tree

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use crypto::sha2::Sha512;
 use crypto::digest::Digest;
+use walkdir::WalkDir;
 
 use std::fs;
 use std::fs::File;
@@ -12,16 +13,15 @@ struct FileInfo {
     size: u64,
     hash: String
 }
-
-fn main() {
-    
-    let args : Vec<String> = std::env::args().collect();
-    let a1 = &args[1];
-
-    let srcdir = PathBuf::from(&a1);
+fn get_file_info(path: &str) -> Option<FileInfo> {
+    let srcdir = PathBuf::from(&path);
     let full_path = fs::canonicalize(&srcdir).expect("File could not be processed");
     let file = File::open(&full_path).expect("can't upen file");
-    let file_length = file.metadata().unwrap().len();
+    let meta = file.metadata().unwrap();
+    if meta.is_dir() {
+        return None;
+    }
+    let file_length = meta.len();
     let mut reader = BufReader::new(file);
 
     let mut hasher = Sha512::new();
@@ -36,15 +36,25 @@ fn main() {
         hasher.input(&buffer[0..count]);
     }    
     
-    let digest = hasher.result_str();
+    let digest = hasher.result_str();    
 
-    println!("digest is {:?}",&digest);
-
-    let f = FileInfo {
+    Some(FileInfo {
         full_path : full_path.to_str().expect("Path could not be translated").to_string(),
         size : file_length,
         hash : digest
-    };
-    println!("{:?}", f);
+    })
+}
+fn process_path(path: &str) {
+    
+    for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
+        if !entry.file_type().is_dir() {
+            println!("{:?}",  get_file_info(entry.path().to_str().unwrap()));
+        }
+        
+    }
+}
+fn main() {
+    
+    process_path(".");
 
 }
